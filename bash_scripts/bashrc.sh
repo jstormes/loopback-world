52486e10fdfc49572bcff70914e00209bff69c33
James Stormes <jstormes@stormes.net>
2018-03-02 10:40:22 -0600

    Adding ssh key copy and path.

--- bash_scripts/bashrc.sh
+++ bash_scripts/bashrc.sh
@@ -19,8 +19,26 @@
 # alias cp='cp -i'
 # alias mv='mv -i'
 
+# Copy SSH key into local setup.
+if [ ! -f ~/ssh/id_rsa ]; then
+    sleep 20
+    echo "################################"
+    echo "# SSH Key not found!!!"
+    echo "# file not found: \\var\\www\\_ssh\\id_rsa"
+    echo "# Copy a valid id_rsh key file into _ssh if you need to use secure git."
+    echo "################################"
+    exit
+else
+    if [ ! -f ~/.ssh/id_rsa ]; then
+        mkdir ~/.ssh
+        # Force Unix line endings.
+        sed -e 's/\r\n/\n/g' ~/ssh/id_rsa > ~/.ssh/id_rsa
+        chmod -R 400 ~/.ssh
+        echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+    fi
+fi
 
-#copy_ssh.sh
+export PATH ="/var/www/_docker/bin:$PATH"
 
 
 echo
