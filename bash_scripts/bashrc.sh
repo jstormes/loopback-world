89544cce0edc7713ef42ffc08164cb4639a64832
James Stormes <jstormes@stormes.net>
2018-03-26 18:27:49 -0500

    Removing dircolors

--- bash_scripts/bashrc.sh
+++ bash_scripts/bashrc.sh
@@ -10,7 +10,7 @@ PS1='Docker:$(hostname -i):\w\$ '
 # You may uncomment the following lines if you want `ls' to be colorized:
 export LS_OPTIONS='--color=auto'
 export LS_COLORS=$LS_COLORS:'di=1;33:'
-eval "`dircolors`"
+#eval "`dircolors`"
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -lA'
