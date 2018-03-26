d03d736957cb0b6693250540e4ac241416d55307
James Stormes <jstormes@stormes.net>
2018-03-26 14:39:37 -0500

    better directory color.

--- bash_scripts/bashrc.sh
+++ bash_scripts/bashrc.sh
@@ -9,6 +9,7 @@ PS1='${debian_chroot:+($debian_chroot)}\h:\w\$ '
 
 # You may uncomment the following lines if you want `ls' to be colorized:
 export LS_OPTIONS='--color=auto'
+export LS_COLORS=$LS_COLORS:'di=1;33:'
 eval "`dircolors`"
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
